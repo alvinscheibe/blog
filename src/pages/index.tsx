c530d77f46008ccc4990289b90abd0b14e02b800
Alvin Scheibe <alvinscheibe@MacBook-Pro-de-Alvin.local>
2022-08-21 13:09:46 -0300

    Tailwind configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className={'text-3xl font-bold underline'}>H1</h1>
+    </>
+  )
+}
+
+export default Home

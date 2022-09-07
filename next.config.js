cf67a06a6d835ad23a16834127d37ee6581fb292
Alvin Scheibe <alvinscheibe@MacBook-Pro-de-Alvin.local>
2022-09-07 15:43:45 -0300

    Fix configs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['images.unsplash.com', 'media.graphassets.com']
+  },
+  env: {
+    API_ENVIRONMENT_GRAPHQL: process.env.NEXT_PUBLIC_API_ENVIRONMENT_GRAPHQL
   }
 }
 

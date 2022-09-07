e4d6e8bfc78ce306d3d4e17960c3357e3f8a64fb
Alvin Scheibe <alvin.scheibe@gmail.com>
2022-09-07 20:03:37 -0300

    Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel={'icon'} type={'image/png'} sizes={'32x32'} href={'/favicon.png'} />
+        </Head>
         <body className={'text-base text-black dark:text-white dark:bg-slate-900'}>
           <Main />
           <NextScript />

fde1c5533fd8819ed00b444bd276bd4fa54a5eeb
Alvin Scheibe <alvin.scheibe@gmail.com>
2022-09-12 18:12:54 -0300

    Fix CustomCode component

--- src/components/CustomCode/index.tsx
+++ src/components/CustomCode/index.tsx
@@ -1,14 +1,14 @@
 import { ReactMarkdownProps } from 'react-markdown/lib/complex-types';
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const CustomCode = ({ node, className, children, ...props }:  Pick<DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>, "key" | keyof HTMLAttributes<HTMLElement>> & ReactMarkdownProps) => {
   const match = /language-(\w+)/.exec(className || '');
 
   return match ? (
     <SyntaxHighlighter
-      style={dracula}
+      style={materialDark}
       language={match[1]}
       showLineNumbers={match && match[1] !== 'bash'}
     >

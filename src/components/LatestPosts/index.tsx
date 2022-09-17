ccffae741c0b7a4b19138e7edd6e6a378995d9f5
Alvin Scheibe <alvin.scheibe@gmail.com>
2022-09-17 10:57:59 -0300

    Fix latest posts component

--- src/components/LatestPosts/index.tsx
+++ src/components/LatestPosts/index.tsx
@@ -8,12 +8,12 @@ interface LatestPostsProps {
 export function LatestPosts({ data }: LatestPostsProps) {
   return (
     <>
-      <div className="grid grid-cols-1 pb-8 text-center">
-        <h6 className="text-indigo-600 text-sm font-bold uppercase mb-2">Blogs</h6>
-        <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">Latest posts</h3>
+      <div className={'grid grid-cols-1 pb-8 text-center'}>
+        <h6 className={'text-indigo-600 text-sm font-bold uppercase mb-2'}>Blogs</h6>
+        <h3 className={'mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold'}>Latest posts</h3>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-[30px]">
+      <div className={'grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-[30px]'}>
         {data?.posts.map((post) => (
           <PostCard key={post.slug} post={{
             title: post.title,

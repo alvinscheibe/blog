1d85ad8857756c63bc9709b7ea14c5a32451e9e4
Alvin Scheibe <alvin.scheibe@gmail.com>
2022-09-17 11:31:40 -0300

    Fix components

--- src/components/PostCard/PostTags.tsx
+++ src/components/PostCard/PostTags.tsx
@@ -8,7 +8,7 @@ export default function PostTags({ tags }: PostTagsProps) {
       {tags.map((tag) => {
         return (
           <li key={tag} style={{display: 'inline-block'}} className={'mr-4'}>
-            <a href={`/tag/${tag}`} className="text-xs text-indigo-600">{tag}</a>
+            <a href={`/tag/${tag}`} className={'text-xs text-indigo-600'}>{tag}</a>
           </li>
         );
       })}
--- src/components/Sections/SectionHeader.tsx
+++ src/components/Sections/SectionHeader.tsx
@@ -4,10 +4,10 @@ interface SectionHeaderProps {
 
 export function SectionHeader({ title }: SectionHeaderProps) {
   return (
-    <section className="relative table w-full py-12 lg:py-16 bg-indigo-500 dark:bg-indigo-900 bg-blog-header-background bg-no-repeat bg-center">
-      <div className="container">
-        <div className="grid grid-cols-1 text-center mt-20 lg:mt-10">
-          <h3 className="md:text-4xl text-3xl md:leading-normal leading-normal font-medium text-white">
+    <section className={'relative table w-full py-12 lg:py-16 bg-indigo-500 dark:bg-indigo-900 bg-blog-header-background bg-no-repeat bg-center'}>
+      <div className={'container'}>
+        <div className={'grid grid-cols-1 text-center mt-20 lg:mt-10'}>
+          <h3 className={'md:text-4xl text-3xl md:leading-normal leading-normal font-medium text-white'}>
             {title}
           </h3>
         </div>

2be7b6403d7f40e68996baa6372de04273158a1d
Alvin Scheibe <alvin.scheibe@gmail.com>
2022-09-17 11:29:00 -0300

    Fix post card component

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -18,19 +18,19 @@ type PostCardProps = {
 
 export default function PostCard({ post }: PostCardProps) {
   return (
-    <div className="blog relative rounded-md shadow dark:shadow-gray-800 overflow-hidden">
+    <div className={'blog relative rounded-md shadow dark:shadow-gray-800 overflow-hidden'}>
       <img src={post.imageUrl?? ''} />
 
-      <div className="content p-6">
+      <div className={'content p-6'}>
         <PostTags tags={post.tags} />
-        <a href={post.href} className="title h5 text-lg font-medium hover:text-indigo-600 duration-500 ease-in-out">
+        <a href={post.href} className={'title h5 text-lg font-medium hover:text-indigo-600 duration-500 ease-in-out'}>
           {post.title}
         </a>
-        <p className="text-slate-400 mt-3">
+        <p className={'text-slate-400 mt-3'}>
           {post.description}
         </p>
-        <div className="mt-4">
-          <a href={post.href} className="btn btn-link font-normal hover:text-indigo-600 after:bg-indigo-600 duration-500 ease-in-out">
+        <div className={'mt-4'}>
+          <a href={post.href} className={'btn btn-link font-normal hover:text-indigo-600 after:bg-indigo-600 duration-500 ease-in-out'}>
             Read More <ArrowRightIcon className={'h-5 w-5 inline'} />
           </a>
         </div>

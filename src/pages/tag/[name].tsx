6449dbc9c20363ac5070df9fdc0eb981bee032b2
Alvin Scheibe <alvin.scheibe@gmail.com>
2022-09-19 19:41:24 -0300

    Fix tag page

--- src/pages/tag/[name].tsx
+++ src/pages/tag/[name].tsx
@@ -49,9 +49,9 @@ const TagPosts: NextPage<ITagPosts> = ({ name }) => {
         </div>
 
         {data && data?.posts.length < 1?
-          <div className="container md:mt-12 mt-8">
-            <div className="grid grid-cols-1 text-center">
-              <h3 className="md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">
+          <div className={'container md:mt-12 mt-8'}>
+            <div className={'grid grid-cols-1 text-center'}>
+              <h3 className={'md:text-3xl md:leading-normal text-2xl leading-normal font-semibold'}>
                 No posts were found for this tag...
               </h3>
             </div>
